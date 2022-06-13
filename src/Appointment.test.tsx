7496f252717b06e01ff48d36dcefe27c10d4fd22
a.kilyushin <a.kilyushin@rambler-co.ru>
2022-06-13 12:36:28 +0400

    Extract appointments definition

--- src/Appointment.test.tsx
+++ src/Appointment.test.tsx
@@ -42,6 +42,10 @@ describe("Appointment", () => {
 })
 
 describe("AppointmentsDayView", () => {
+  const today = new Date()
+  today.setMinutes(0)
+  const appointments = [{ startsAt: today.setHours(12) }, { startsAt: today.setHours(13) }]
+
   let container: HTMLDivElement
 
   beforeEach(() => {
@@ -62,9 +66,7 @@ describe("AppointmentsDayView", () => {
   })
 
   it("renders multiple appointments in an `ol` element.", async () => {
-    const today = new Date()
-
-    render(<AppointmentsDayView appointments={[{ startsAt: today.setHours(12) }, { startsAt: today.setHours(13) }]} />)
+    render(<AppointmentsDayView appointments={appointments} />)
 
     await wait()
 
@@ -75,10 +77,7 @@ describe("AppointmentsDayView", () => {
   })
 
   it("renders multiple `li` with time childrend in an `ol` element.", async () => {
-    const today = new Date()
-    today.setMinutes(0)
-
-    render(<AppointmentsDayView appointments={[{ startsAt: today.setHours(12) }, { startsAt: today.setHours(13) }]} />)
+    render(<AppointmentsDayView appointments={appointments} />)
 
     await wait()
 
@@ -87,8 +86,8 @@ describe("AppointmentsDayView", () => {
     assert(appointmentsList !== null, "appointmentsList is `null`")
     expect(appointmentsList.children).toHaveLength(2)
 
-    const appointments = appointmentsList.querySelectorAll("li")
-    expect(appointments[0].textContent).toMatch("12:00")
-    expect(appointments[1].textContent).toMatch("13:00")
+    const appointmentsNodes = appointmentsList.querySelectorAll("li")
+    expect(appointmentsNodes[0].textContent).toMatch("12:00")
+    expect(appointmentsNodes[1].textContent).toMatch("13:00")
   })
 })
