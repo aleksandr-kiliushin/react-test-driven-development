a4f91461f29ae8c09908907e5df771aefdbcc457
a.kilyushin <a.kilyushin@rambler-co.ru>
2022-06-13 23:18:19 +0400

    Select renders

--- src/AppointmentForm/index.test.tsx
+++ src/AppointmentForm/index.test.tsx
@@ -5,7 +5,7 @@ import ReactDom from "react-dom/client"
 import createContainer from "#utils/testing/createContainer"
 import wait from "#utils/testing/wait"
 
-import AppointmentForm from "./index"
+import AppointmentForm, { FieldName } from "./index"
 
 describe("AppointmentForm", () => {
   let container: HTMLDivElement
@@ -17,13 +17,28 @@ describe("AppointmentForm", () => {
 
   const findForm = ({ id }: { id: "appointment" }): HTMLFormElement => {
     const form = container.querySelector(`form#${id}`)
+    assert(form !== null, "Expected to find a form instance, but found `null`")
     assert(form instanceof HTMLFormElement, "Found element is not a form.")
     return form
+  }
+
+  const findSelectField = ({ fieldName }: { fieldName: FieldName }): HTMLSelectElement => {
+    const field = findForm({ id: "appointment" }).elements.namedItem(fieldName)
+    assert(field instanceof HTMLSelectElement, "firstNameField is not a select")
+    return field
   }
 
   it("renders a form.", async () => {
     render(<AppointmentForm />)
     await wait()
     expect(findForm({ id: "appointment" })).not.toBeNull()
+  })
+
+  describe("service field", () => {
+    it("renders as a select box", async () => {
+      render(<AppointmentForm />)
+      await wait()
+      expect(findSelectField({ fieldName: "serviceName" })).not.toBeNull()
+    })
   })
 })
--- src/AppointmentForm/index.tsx
+++ src/AppointmentForm/index.tsx
@@ -1,9 +1,15 @@
 import React from "react"
 
+export type FieldName = "serviceName"
+
 type Props = {}
 
 const AppointmentForm: React.FC<Props> = () => {
-  return <form id="appointment"></form>
+  return (
+    <form id="appointment">
+      <select name="serviceName"></select>
+    </form>
+  )
 }
 
 export default AppointmentForm
