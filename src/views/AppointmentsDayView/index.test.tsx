a67cecfe0d6e37c7f5f70f9254b7d11a58a89955
a.kilyushin <a.kilyushin@rambler-co.ru>
2022-06-15 17:06:05 +0400

    ch2-cleanup: Refactor AppointmentsDayView 2

--- src/views/AppointmentsDayView/index.test.tsx
+++ src/views/AppointmentsDayView/index.test.tsx
@@ -18,23 +18,18 @@ describe("AppointmentsDayView", () => {
     ;({ container, render } = createContainer())
   })
 
-  const today = new Date()
-  today.setMinutes(0)
   const appointments: IAppointment[] = [anAppointment1, anAppointment2]
 
   it("renders a ol with the right className.", async () => {
     render(<AppointmentsDayView appointments={[]} />)
     await wait()
-
     expect(container.querySelector("ol.appointmentsDayView")).not.toEqual(null)
   })
 
   it("renders multiple appointments in an `ol` element.", async () => {
     render(<AppointmentsDayView appointments={appointments} />)
     await wait()
-
     const appointmentsList = container.querySelector("ol.appointmentsDayView")
-
     assert(appointmentsList !== null, "appointmentsList is `null`")
     expect(appointmentsList.querySelectorAll("li")).toHaveLength(2)
   })
@@ -42,11 +37,8 @@ describe("AppointmentsDayView", () => {
   it("renders multiple `li` with time childrend in an `ol` element.", async () => {
     render(<AppointmentsDayView appointments={appointments} />)
     await wait()
-
     const appointmentsList = container.querySelector("ol.appointmentsDayView")
-
     assert(appointmentsList !== null, "appointmentsList is `null`")
-
     const appointmentsNodes = appointmentsList.querySelectorAll("li")
     expect(appointmentsNodes[0].textContent).toMatch("14:00")
     expect(appointmentsNodes[1].textContent).toMatch("12:00")
@@ -55,7 +47,6 @@ describe("AppointmentsDayView", () => {
   it("renders a ol with the right className.", async () => {
     render(<AppointmentsDayView appointments={[]} />)
     await wait()
-
     const appointmentsList = container.querySelector("ol.appointmentsDayView")
     assert(appointmentsList !== null, "appointmentsList is `null`")
     expect(appointmentsList.textContent).toMatch("There are no appointments scheduled for today.")
@@ -64,7 +55,6 @@ describe("AppointmentsDayView", () => {
   it("selects the first appointment by default.", async () => {
     render(<AppointmentsDayView appointments={appointments} />)
     await wait()
-
     const appointmentsList = container.querySelector("ol.appointmentsDayView")
     assert(appointmentsList !== null, "appointmentsList is `null`")
     expect(appointmentsList.textContent).toMatch("Roscoe")
@@ -73,10 +63,8 @@ describe("AppointmentsDayView", () => {
   it("has a button element in each li.", async () => {
     render(<AppointmentsDayView appointments={appointments} />)
     await wait()
-
     const appointmentsList = container.querySelector("ol.appointmentsDayView")
     assert(appointmentsList !== null, "appointmentsList is `null`")
-
     const buttons = container.querySelectorAll("li > button[type='button']")
     expect(buttons).toHaveLength(2)
   })
@@ -84,12 +72,9 @@ describe("AppointmentsDayView", () => {
   it("renders another appointment when selected", async () => {
     render(<AppointmentsDayView appointments={appointments} />)
     await wait()
-
     const anotherAppointmentButton = container.querySelectorAll("li > button[type='button']")[1]
     ReactDomTestUtils.Simulate.click(anotherAppointmentButton)
-
     await wait()
-
     expect(container.textContent).toMatch("Cara")
   })
 })
