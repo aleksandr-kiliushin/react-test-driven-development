b28c522b4356825ed9fbda2f57c642d62b95e97f
a.kilyushin <a.kilyushin@rambler-co.ru>
2022-06-15 16:33:56 +0400

    ch2-cleanup: Add TODO

--- src/sampleData/randomAppointments.ts
+++ src/sampleData/randomAppointments.ts
@@ -8,7 +8,9 @@ const todayAt = ({ hours }: { hours: number }) => {
   return today.setHours(hours, 0)
 }
 
-export const randomAppointments: IAppointment[] = [10, 11, 12, 13, 14, 15, 16].map<IAppointment>((hours) => ({
+export const TODO_MAKE_DATE_RANDOM_TOO_randomAppointments: IAppointment[] = [
+  10, 11, 12, 13, 14, 15, 16,
+].map<IAppointment>((hours) => ({
   customer: {
     firstName: faker.name.firstName(),
     lastName: faker.name.lastName(),
