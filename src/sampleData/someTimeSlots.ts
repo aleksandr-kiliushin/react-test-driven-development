0b35bc69c20ecc7fc48f56ad92479f7b93a3a84b
a.kilyushin <a.kilyushin@rambler-co.ru>
2022-06-15 22:39:23 +0400

    ch2-improvements: Change sample timeslot in 6 days

--- src/sampleData/someTimeSlots.ts
+++ src/sampleData/someTimeSlots.ts
@@ -17,11 +17,11 @@ aTimeSlotTomorrowAt_12_00.setMinutes(0)
 aTimeSlotTomorrowAt_12_00.setSeconds(0)
 aTimeSlotTomorrowAt_12_00.setMilliseconds(0)
 
-const aTimeSlotInAWeekAt_17_30 = new Date()
-aTimeSlotInAWeekAt_17_30.setDate(new Date().getDate() + 6)
-aTimeSlotInAWeekAt_17_30.setHours(17)
-aTimeSlotInAWeekAt_17_30.setMinutes(30)
-aTimeSlotInAWeekAt_17_30.setSeconds(0)
-aTimeSlotInAWeekAt_17_30.setMilliseconds(0)
+const aTimeSlotIn6DaysAt_13_00 = new Date()
+aTimeSlotIn6DaysAt_13_00.setDate(new Date().getDate() + 6)
+aTimeSlotIn6DaysAt_13_00.setHours(13)
+aTimeSlotIn6DaysAt_13_00.setMinutes(0)
+aTimeSlotIn6DaysAt_13_00.setSeconds(0)
+aTimeSlotIn6DaysAt_13_00.setMilliseconds(0)
 
-export { aTimeSlotTodayAt_12_00, aTimeSlotTodayAt_13_30, aTimeSlotTomorrowAt_12_00, aTimeSlotInAWeekAt_17_30 }
+export { aTimeSlotTodayAt_12_00, aTimeSlotTodayAt_13_30, aTimeSlotTomorrowAt_12_00, aTimeSlotIn6DaysAt_13_00 }
