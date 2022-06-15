9ed453089b351c5a2cd6b29bac8ace11a837729a
a.kilyushin <a.kilyushin@rambler-co.ru>
2022-06-15 17:07:05 +0400

    ch2-cleanup: Refactor Appointment

--- src/views/AppointmentsDayView/Appointment.test.tsx
+++ src/views/AppointmentsDayView/Appointment.test.tsx
@@ -19,7 +19,6 @@ describe("Appointment", () => {
   it("renders an appointment heading", async () => {
     render(<Appointment appointment={anAppointment1} />)
     await wait()
-
     const appointmentHeading = container.querySelector("h2")
     assert(appointmentHeading !== null, "appointmentHeading should not be null")
     expect(appointmentHeading.textContent).toMatch(`Todays appointment at 14:00`)
@@ -28,7 +27,6 @@ describe("Appointment", () => {
   it("renders an appointment heading", async () => {
     render(<Appointment appointment={anAppointment2} />)
     await wait()
-
     const appointmentHeading = container.querySelector("h2")
     assert(appointmentHeading !== null, "appointmentHeading should not be null")
     expect(appointmentHeading.textContent).toMatch(`Todays appointment at 12:00`)
@@ -37,7 +35,6 @@ describe("Appointment", () => {
   it("renders a customer name", async () => {
     render(<Appointment appointment={anAppointment1} />)
     await wait()
-
     expect(container.textContent).toMatch(
       `Customer name: ${anAppointment1.customer.firstName} ${anAppointment1.customer.lastName}`
     )
@@ -46,7 +43,6 @@ describe("Appointment", () => {
   it("renders another customer name", async () => {
     render(<Appointment appointment={anAppointment2} />)
     await wait()
-
     expect(container.textContent).toMatch(
       `Customer name: ${anAppointment2.customer.firstName} ${anAppointment2.customer.lastName}`
     )
@@ -55,42 +51,36 @@ describe("Appointment", () => {
   it("renders a customer phone number", async () => {
     render(<Appointment appointment={anAppointment1} />)
     await wait()
-
     expect(container.textContent).toMatch(`Customer phone: ${anAppointment1.customer.phoneNumber}`)
   })
 
   it("renders another customer phone number", async () => {
     render(<Appointment appointment={anAppointment2} />)
     await wait()
-
     expect(container.textContent).toMatch(`Customer phone: ${anAppointment2.customer.phoneNumber}`)
   })
 
   it("renders a service name", async () => {
     render(<Appointment appointment={anAppointment1} />)
     await wait()
-
     expect(container.textContent).toMatch(`Service: ${anAppointment1.serviceName}`)
   })
 
   it("renders another appointment service name", async () => {
     render(<Appointment appointment={anAppointment2} />)
     await wait()
-
     expect(container.textContent).toMatch(`Service: ${anAppointment2.serviceName}`)
   })
 
   it("renders an appointment notes", async () => {
     render(<Appointment appointment={anAppointment1} />)
     await wait()
-
     expect(container.textContent).toMatch(`Notes: ${anAppointment1.notes}`)
   })
 
   it("renders another appointment notes", async () => {
     render(<Appointment appointment={anAppointment2} />)
     await wait()
-
     expect(container.textContent).toMatch(`Notes: ${anAppointment2.notes}`)
   })
 })
