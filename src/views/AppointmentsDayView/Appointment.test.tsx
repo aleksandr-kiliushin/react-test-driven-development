08ebd4f638e7d70d59178ff520c8822c1ea05b5f
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-06-18 20:54:41 +0400

    chapter3: Get rid of wait() in AppointmentsDayView

--- src/views/AppointmentsDayView/Appointment.test.tsx
+++ src/views/AppointmentsDayView/Appointment.test.tsx
@@ -1,12 +1,14 @@
 import assert from "node:assert"
 import React from "react"
 import ReactDom from "react-dom/client"
+import { act } from "react-dom/test-utils"
 
 import { anAppointment1, anAppointment2 } from "#sampleData/someAppointments"
 import { createContainer } from "#utils/testing/createContainer"
-import { wait } from "#utils/testing/wait"
 
 import { Appointment } from "./Appointment"
+
+globalThis.IS_REACT_ACT_ENVIRONMENT = true // TODO: Move to test setup file.
 
 describe("Appointment", () => {
   let container: HTMLDivElement
@@ -16,71 +18,81 @@ describe("Appointment", () => {
     ;({ container, render } = createContainer())
   })
 
-  it("renders an appointment heading", async () => {
-    render(<Appointment appointment={anAppointment1} />)
-    await wait()
+  it("renders an appointment heading", () => {
+    act(() => {
+      render(<Appointment appointment={anAppointment1} />)
+    })
     const appointmentHeading = container.querySelector("h2")
     assert(appointmentHeading !== null, "appointmentHeading should not be null")
     expect(appointmentHeading.textContent).toMatch(`Todays appointment at 12:00`)
   })
 
-  it("renders an appointment heading", async () => {
-    render(<Appointment appointment={anAppointment2} />)
-    await wait()
+  it("renders an appointment heading", () => {
+    act(() => {
+      render(<Appointment appointment={anAppointment2} />)
+    })
     const appointmentHeading = container.querySelector("h2")
     assert(appointmentHeading !== null, "appointmentHeading should not be null")
     expect(appointmentHeading.textContent).toMatch(`Todays appointment at 12:00`)
   })
 
-  it("renders a customer name", async () => {
-    render(<Appointment appointment={anAppointment1} />)
-    await wait()
+  it("renders a customer name", () => {
+    act(() => {
+      render(<Appointment appointment={anAppointment1} />)
+    })
     expect(container.textContent).toMatch(
       `Customer name: ${anAppointment1.customer.firstName} ${anAppointment1.customer.lastName}`
     )
   })
 
-  it("renders another customer name", async () => {
-    render(<Appointment appointment={anAppointment2} />)
-    await wait()
+  it("renders another customer name", () => {
+    act(() => {
+      render(<Appointment appointment={anAppointment2} />)
+    })
     expect(container.textContent).toMatch(
       `Customer name: ${anAppointment2.customer.firstName} ${anAppointment2.customer.lastName}`
     )
   })
 
-  it("renders a customer phone number", async () => {
-    render(<Appointment appointment={anAppointment1} />)
-    await wait()
+  it("renders a customer phone number", () => {
+    act(() => {
+      render(<Appointment appointment={anAppointment1} />)
+    })
     expect(container.textContent).toMatch(`Customer phone: ${anAppointment1.customer.phoneNumber}`)
   })
 
-  it("renders another customer phone number", async () => {
-    render(<Appointment appointment={anAppointment2} />)
-    await wait()
+  it("renders another customer phone number", () => {
+    act(() => {
+      render(<Appointment appointment={anAppointment2} />)
+    })
     expect(container.textContent).toMatch(`Customer phone: ${anAppointment2.customer.phoneNumber}`)
   })
 
-  it("renders a service name", async () => {
-    render(<Appointment appointment={anAppointment1} />)
-    await wait()
+  it("renders a service name", () => {
+    act(() => {
+      render(<Appointment appointment={anAppointment1} />)
+    })
     expect(container.textContent).toMatch(`Service: ${anAppointment1.serviceName}`)
   })
 
-  it("renders another appointment service name", async () => {
-    render(<Appointment appointment={anAppointment2} />)
-    await wait()
+  it("renders another appointment service name", () => {
+    act(() => {
+      render(<Appointment appointment={anAppointment2} />)
+    })
     expect(container.textContent).toMatch(`Service: ${anAppointment2.serviceName}`)
   })
 
-  it("renders an appointment notes", async () => {
-    render(<Appointment appointment={anAppointment1} />)
-    await wait()
+  it("renders an appointment notes", () => {
+    act(() => {
+      render(<Appointment appointment={anAppointment1} />)
+    })
     expect(container.textContent).toMatch(`Notes: ${anAppointment1.notes}`)
   })
 
-  it("renders another appointment notes", async () => {
-    render(<Appointment appointment={anAppointment2} />)
-    await wait()
+  it("renders another appointment notes", () => {
+    act(() => {
+      render(<Appointment appointment={anAppointment2} />)
+    })
     expect(container.textContent).toMatch(`Notes: ${anAppointment2.notes}`)
   })
 })
--- src/views/AppointmentsDayView/index.test.tsx
+++ src/views/AppointmentsDayView/index.test.tsx
@@ -1,14 +1,15 @@
 import assert from "node:assert"
 import React from "react"
 import ReactDom from "react-dom/client"
-import ReactDomTestUtils from "react-dom/test-utils"
+import ReactDomTestUtils, { act } from "react-dom/test-utils"
 
 import { anAppointment1, anAppointment2 } from "#sampleData/someAppointments"
 import { IAppointment } from "#types/IAppointment"
 import { createContainer } from "#utils/testing/createContainer"
-import { wait } from "#utils/testing/wait"
 
 import { AppointmentsDayView } from "./index"
+
+globalThis.IS_REACT_ACT_ENVIRONMENT = true // TODO: Move to test setup file.
 
 describe("AppointmentsDayView", () => {
   let container: HTMLDivElement
@@ -20,23 +21,26 @@ describe("AppointmentsDayView", () => {
 
   const appointments: IAppointment[] = [anAppointment1, anAppointment2]
 
-  it("renders a ol with the right className.", async () => {
-    render(<AppointmentsDayView appointments={[]} />)
-    await wait()
+  it("renders a ol with the right className.", () => {
+    act(() => {
+      render(<AppointmentsDayView appointments={[]} />)
+    })
     expect(container.querySelector("ol.appointmentsDayView")).not.toEqual(null)
   })
 
-  it("renders multiple appointments in an `ol` element.", async () => {
-    render(<AppointmentsDayView appointments={appointments} />)
-    await wait()
+  it("renders multiple appointments in an `ol` element.", () => {
+    act(() => {
+      render(<AppointmentsDayView appointments={appointments} />)
+    })
     const appointmentsList = container.querySelector("ol.appointmentsDayView")
     assert(appointmentsList !== null, "appointmentsList is `null`")
     expect(appointmentsList.querySelectorAll("li")).toHaveLength(2)
   })
 
-  it("renders multiple `li` with time childrend in an `ol` element.", async () => {
-    render(<AppointmentsDayView appointments={appointments} />)
-    await wait()
+  it("renders multiple `li` with time childrend in an `ol` element.", () => {
+    act(() => {
+      render(<AppointmentsDayView appointments={appointments} />)
+    })
     const appointmentsList = container.querySelector("ol.appointmentsDayView")
     assert(appointmentsList !== null, "appointmentsList is `null`")
     const appointmentsNodes = appointmentsList.querySelectorAll("li")
@@ -44,33 +48,38 @@ describe("AppointmentsDayView", () => {
     expect(appointmentsNodes[1].textContent).toMatch("12:00")
   })
 
-  it("renders an empty appointments text when passed an empty appointments array.", async () => {
-    render(<AppointmentsDayView appointments={[]} />)
-    await wait()
+  it("renders an empty appointments text when passed an empty appointments array.", () => {
+    act(() => {
+      render(<AppointmentsDayView appointments={[]} />)
+    })
     expect(container.textContent).toMatch("There are no appointments scheduled for today.")
   })
 
-  it("selects the first appointment by default.", async () => {
-    render(<AppointmentsDayView appointments={appointments} />)
-    await wait()
+  it("selects the first appointment by default.", () => {
+    act(() => {
+      render(<AppointmentsDayView appointments={appointments} />)
+    })
     expect(container.textContent).toMatch("Roscoe")
   })
 
-  it("has a button element in each li.", async () => {
-    render(<AppointmentsDayView appointments={appointments} />)
-    await wait()
+  it("has a button element in each li.", () => {
+    act(() => {
+      render(<AppointmentsDayView appointments={appointments} />)
+    })
     const appointmentsList = container.querySelector("ol.appointmentsDayView")
     assert(appointmentsList !== null, "appointmentsList is `null`")
     const buttons = container.querySelectorAll("li > button[type='button']")
     expect(buttons).toHaveLength(2)
   })
 
-  it("renders another appointment when selected", async () => {
-    render(<AppointmentsDayView appointments={appointments} />)
-    await wait()
+  it("renders another appointment when selected", () => {
+    act(() => {
+      render(<AppointmentsDayView appointments={appointments} />)
+    })
     const anotherAppointmentButton = container.querySelectorAll("li > button[type='button']")[1]
-    ReactDomTestUtils.Simulate.click(anotherAppointmentButton)
-    await wait()
+    act(() => {
+      ReactDomTestUtils.Simulate.click(anotherAppointmentButton)
+    })
     expect(container.textContent).toMatch("Cara")
   })
 })
