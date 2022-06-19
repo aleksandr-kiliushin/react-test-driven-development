1c2da884f69d87410bc98d3e3dd37cb1c636b9e8
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-06-19 21:36:01 +0400

    chapter3: AppointmentFormLoader mocks AppointmentFormExports.AppointmentForm

--- src/views/AppointmentForm/AppointmentFormLoader.test.tsx
+++ src/views/AppointmentForm/AppointmentFormLoader.test.tsx
@@ -5,28 +5,33 @@ import { IRenderContainer, createContainer } from "#utils/testing/createContaine
 import { createFetchSuccessfulResponse } from "#utils/testing/spyHelpers"
 
 import { AppointmentFormLoader } from "./AppointmentFormLoader"
+import * as AppointmentFormExports from "./index"
 
 globalThis.IS_REACT_ACT_ENVIRONMENT = true // TODO: Move to test setup file.
 
 type IAppointmentFormLoaderRenderContainer = IRenderContainer<{ formIds: []; fieldNames: [] }>
 
+const today = new Date()
+const availableTimeSlots = [{ startsAt: today.setHours(9, 0, 0, 0) }]
+
 describe("AppointmentFormLoader", () => {
   let render: IAppointmentFormLoaderRenderContainer["render"]
 
-  const today = new Date()
-  const availableTimeSlots = [{ startsAt: today.setHours(9, 0, 0, 0) }]
   beforeEach(() => {
     ;({ render } = createContainer())
     // @ts-ignore
-    jest.spyOn(window, "fetch").mockReturnValue(createFetchSuccessfulResponse(availableTimeSlots))
+    jest.spyOn(globalThis, "fetch").mockReturnValue(createFetchSuccessfulResponse(availableTimeSlots))
+    jest.spyOn(AppointmentFormExports, "AppointmentForm").mockReturnValue(null)
   })
+
   afterEach(() => {
-    ;(window.fetch as jest.Mock).mockRestore()
+    ;(globalThis.fetch as jest.Mock).mockRestore()
+    ;(AppointmentFormExports.AppointmentForm as jest.Mock).mockRestore()
   })
 
   it("fetches data when component is mounted", () => {
     render(<AppointmentFormLoader />)
-    expect(window.fetch).toHaveBeenCalledWith(
+    expect(globalThis.fetch).toHaveBeenCalledWith(
       "/availableTimeSlots",
       expect.objectContaining({
         credentials: "same-origin",
--- src/views/AppointmentForm/AppointmentFormLoader.tsx
+++ src/views/AppointmentForm/AppointmentFormLoader.tsx
@@ -3,7 +3,7 @@ import React from "react"
 export const AppointmentFormLoader = () => {
   React.useEffect(() => {
     const fetchAvailableTimeSlots = () => {
-      window.fetch("/availableTimeSlots", {
+      globalThis.fetch("/availableTimeSlots", {
         credentials: "same-origin",
         headers: { "Content-Type": "application/json" },
         method: "GET",
