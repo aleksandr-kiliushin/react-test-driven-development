e200a207a1ef938d3c2938e23985e0e82cd2a7c3
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-06-19 17:48:36 +0400

    chapter3: Refactor AppointmentsDayView tests

--- src/views/AppointmentsDayView/Appointment.test.tsx
+++ src/views/AppointmentsDayView/Appointment.test.tsx
@@ -1,98 +1,78 @@
 import assert from "node:assert"
 import React from "react"
-import ReactDom from "react-dom/client"
-import { act } from "react-dom/test-utils"
 
 import { anAppointment1, anAppointment2 } from "#sampleData/someAppointments"
-import { createContainer } from "#utils/testing/createContainer"
+import { IRenderContainer, createContainer } from "#utils/testing/createContainer"
 
 import { Appointment } from "./Appointment"
 
 globalThis.IS_REACT_ACT_ENVIRONMENT = true // TODO: Move to test setup file.
 
+type IAppointmentRenderContainer = IRenderContainer<{ formIds: []; fieldNames: [] }>
+
 describe("Appointment", () => {
-  let container: HTMLDivElement
-  let render: ReactDom.Root["render"]
+  let container: IAppointmentRenderContainer["container"]
+  let render: IAppointmentRenderContainer["render"]
 
   beforeEach(() => {
     ;({ container, render } = createContainer())
   })
 
   it("renders an appointment heading", () => {
-    act(() => {
-      render(<Appointment appointment={anAppointment1} />)
-    })
+    render(<Appointment appointment={anAppointment1} />)
     const appointmentHeading = container.querySelector("h2")
     assert(appointmentHeading !== null, "appointmentHeading should not be null")
     expect(appointmentHeading.textContent).toMatch(`Todays appointment at 12:00`)
   })
 
   it("renders an appointment heading", () => {
-    act(() => {
-      render(<Appointment appointment={anAppointment2} />)
-    })
+    render(<Appointment appointment={anAppointment2} />)
     const appointmentHeading = container.querySelector("h2")
     assert(appointmentHeading !== null, "appointmentHeading should not be null")
     expect(appointmentHeading.textContent).toMatch(`Todays appointment at 12:00`)
   })
 
   it("renders a customer name", () => {
-    act(() => {
-      render(<Appointment appointment={anAppointment1} />)
-    })
+    render(<Appointment appointment={anAppointment1} />)
     expect(container.textContent).toMatch(
       `Customer name: ${anAppointment1.customer.firstName} ${anAppointment1.customer.lastName}`
     )
   })
 
   it("renders another customer name", () => {
-    act(() => {
-      render(<Appointment appointment={anAppointment2} />)
-    })
+    render(<Appointment appointment={anAppointment2} />)
     expect(container.textContent).toMatch(
       `Customer name: ${anAppointment2.customer.firstName} ${anAppointment2.customer.lastName}`
     )
   })
 
   it("renders a customer phone number", () => {
-    act(() => {
-      render(<Appointment appointment={anAppointment1} />)
-    })
+    render(<Appointment appointment={anAppointment1} />)
     expect(container.textContent).toMatch(`Customer phone: ${anAppointment1.customer.phoneNumber}`)
   })
 
   it("renders another customer phone number", () => {
-    act(() => {
-      render(<Appointment appointment={anAppointment2} />)
-    })
+    render(<Appointment appointment={anAppointment2} />)
     expect(container.textContent).toMatch(`Customer phone: ${anAppointment2.customer.phoneNumber}`)
   })
 
   it("renders a service name", () => {
-    act(() => {
-      render(<Appointment appointment={anAppointment1} />)
-    })
+    render(<Appointment appointment={anAppointment1} />)
     expect(container.textContent).toMatch(`Service: ${anAppointment1.serviceName}`)
   })
 
   it("renders another appointment service name", () => {
-    act(() => {
-      render(<Appointment appointment={anAppointment2} />)
-    })
+    render(<Appointment appointment={anAppointment2} />)
     expect(container.textContent).toMatch(`Service: ${anAppointment2.serviceName}`)
   })
 
   it("renders an appointment notes", () => {
-    act(() => {
-      render(<Appointment appointment={anAppointment1} />)
-    })
+    render(<Appointment appointment={anAppointment1} />)
     expect(container.textContent).toMatch(`Notes: ${anAppointment1.notes}`)
   })
 
   it("renders another appointment notes", () => {
-    act(() => {
-      render(<Appointment appointment={anAppointment2} />)
-    })
+    render(<Appointment appointment={anAppointment2} />)
     expect(container.textContent).toMatch(`Notes: ${anAppointment2.notes}`)
   })
 })
--- src/views/AppointmentsDayView/index.test.tsx
+++ src/views/AppointmentsDayView/index.test.tsx
@@ -1,85 +1,68 @@
 import assert from "node:assert"
 import React from "react"
-import ReactDom from "react-dom/client"
-import ReactDomTestUtils, { act } from "react-dom/test-utils"
 
 import { anAppointment1, anAppointment2 } from "#sampleData/someAppointments"
 import { IAppointment } from "#types/IAppointment"
-import { createContainer } from "#utils/testing/createContainer"
+import { IRenderContainer, createContainer } from "#utils/testing/createContainer"
 
 import { AppointmentsDayView } from "./index"
 
 globalThis.IS_REACT_ACT_ENVIRONMENT = true // TODO: Move to test setup file.
 
+type IAppointmentsDayViewFormRenderContainer = IRenderContainer<{ formIds: []; fieldNames: [] }>
+
 describe("AppointmentsDayView", () => {
-  let container: HTMLDivElement
-  let render: ReactDom.Root["render"]
+  let container: IAppointmentsDayViewFormRenderContainer["container"]
+  let findElement: IAppointmentsDayViewFormRenderContainer["findElement"]
+  let findElements: IAppointmentsDayViewFormRenderContainer["findElements"]
+  let render: IAppointmentsDayViewFormRenderContainer["render"]
+  let simulateClick: IAppointmentsDayViewFormRenderContainer["simulateClick"]
 
   beforeEach(() => {
-    ;({ container, render } = createContainer())
+    ;({ container, findElement, findElements, render, simulateClick } = createContainer())
   })
 
   const appointments: IAppointment[] = [anAppointment1, anAppointment2]
 
   it("renders a ol with the right className.", () => {
-    act(() => {
-      render(<AppointmentsDayView appointments={[]} />)
-    })
-    expect(container.querySelector("ol.appointmentsDayView")).not.toEqual(null)
+    render(<AppointmentsDayView appointments={[]} />)
+    expect(findElement("ol.appointmentsDayView")).not.toEqual(null)
   })
 
   it("renders multiple appointments in an `ol` element.", () => {
-    act(() => {
-      render(<AppointmentsDayView appointments={appointments} />)
-    })
-    const appointmentsList = container.querySelector("ol.appointmentsDayView")
-    assert(appointmentsList !== null, "appointmentsList is `null`")
-    expect(appointmentsList.querySelectorAll("li")).toHaveLength(2)
+    render(<AppointmentsDayView appointments={appointments} />)
+    const appointmentsList = findElement("ol.appointmentsDayView")
+    assert(appointmentsList !== null, "AppointmentsList is not found.")
+    expect(findElements("li")).toHaveLength(2)
   })
 
   it("renders multiple `li` with time childrend in an `ol` element.", () => {
-    act(() => {
-      render(<AppointmentsDayView appointments={appointments} />)
-    })
-    const appointmentsList = container.querySelector("ol.appointmentsDayView")
-    assert(appointmentsList !== null, "appointmentsList is `null`")
-    const appointmentsNodes = appointmentsList.querySelectorAll("li")
+    render(<AppointmentsDayView appointments={appointments} />)
+    const appointmentsNodes = findElements("li")
     expect(appointmentsNodes[0].textContent).toMatch("12:00")
     expect(appointmentsNodes[1].textContent).toMatch("12:00")
   })
 
   it("renders an empty appointments text when passed an empty appointments array.", () => {
-    act(() => {
-      render(<AppointmentsDayView appointments={[]} />)
-    })
+    render(<AppointmentsDayView appointments={[]} />)
     expect(container.textContent).toMatch("There are no appointments scheduled for today.")
   })
 
   it("selects the first appointment by default.", () => {
-    act(() => {
-      render(<AppointmentsDayView appointments={appointments} />)
-    })
+    render(<AppointmentsDayView appointments={appointments} />)
     expect(container.textContent).toMatch("Roscoe")
   })
 
   it("has a button element in each li.", () => {
-    act(() => {
-      render(<AppointmentsDayView appointments={appointments} />)
-    })
-    const appointmentsList = container.querySelector("ol.appointmentsDayView")
-    assert(appointmentsList !== null, "appointmentsList is `null`")
-    const buttons = container.querySelectorAll("li > button[type='button']")
+    render(<AppointmentsDayView appointments={appointments} />)
+    const buttons = findElements("li > button[type='button']")
     expect(buttons).toHaveLength(2)
   })
 
   it("renders another appointment when selected", () => {
-    act(() => {
-      render(<AppointmentsDayView appointments={appointments} />)
-    })
-    const anotherAppointmentButton = container.querySelectorAll("li > button[type='button']")[1]
-    act(() => {
-      ReactDomTestUtils.Simulate.click(anotherAppointmentButton)
-    })
+    render(<AppointmentsDayView appointments={appointments} />)
+    const anotherAppointmentButton = findElements("li > button[type='button']")[1]
+    simulateClick(anotherAppointmentButton)
     expect(container.textContent).toMatch("Cara")
   })
 })
