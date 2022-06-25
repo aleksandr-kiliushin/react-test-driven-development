f17772433b8bcf3f46446dda53bc79a4233674f3
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-06-25 22:35:42 +0400

    chapter-7: App renders customers search and add customer buttons

---
+++ src/views/App.test.tsx
@@ -0,0 +1,54 @@
+import React from "react"
+import "whatwg-fetch"
+
+import { IRenderContainer, createContainer } from "#utils/testing/createContainer"
+
+import { App } from "./App"
+
+globalThis.IS_REACT_ACT_ENVIRONMENT = true // TODO: Move to test setup file.
+
+type ICustomerSearchRenderContainer = IRenderContainer<{ formIds: []; fieldNames: [] }>
+
+describe("App", () => {
+  let findElement: ICustomerSearchRenderContainer["findElement"]
+  // let findElements: ICustomerSearchRenderContainer["findElements"]
+  // let findField: ICustomerSearchRenderContainer["findField"]
+  // let findFieldLabel: ICustomerSearchRenderContainer["findFieldLabel"]
+  // let findForm: ICustomerSearchRenderContainer["findForm"]
+  let render: ICustomerSearchRenderContainer["render"]
+  // let renderAndWait: ICustomerSearchRenderContainer["renderAndWait"]
+  // let simulateBlur: ICustomerSearchRenderContainer["simulateBlur"]
+  // let simulateChange: ICustomerSearchRenderContainer["simulateChange"]
+  // let simulateChangeAndWait: ICustomerSearchRenderContainer["simulateChangeAndWait"]
+  // let simulateClickAndWait: ICustomerSearchRenderContainer["simulateClickAndWait"]
+  // let simulateSubmit: ICustomerSearchRenderContainer["simulateSubmit"]
+  // let simulateSubmitAndWait: ICustomerSearchRenderContainer["simulateSubmitAndWait"]
+
+  beforeEach(() => {
+    ;({
+      findElement,
+      // findElements,
+      // findField,
+      // findFieldLabel,
+      // findForm,
+      render,
+      // renderAndWait,
+      // simulateBlur,
+      // simulateChange,
+      // simulateChangeAndWait,
+      // simulateClickAndWait,
+      // simulateSubmit,
+      // simulateSubmitAndWait,
+    } = createContainer())
+  })
+
+  it("renders the add customer button", () => {
+    render(<App />)
+    expect(findElement("button#addCustomer")).not.toBeNull()
+  })
+
+  it("renders the search customers button", () => {
+    render(<App />)
+    expect(findElement("button#searchCustomers")).not.toBeNull()
+  })
+})
