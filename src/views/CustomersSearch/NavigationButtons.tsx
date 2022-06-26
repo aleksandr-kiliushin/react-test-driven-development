25bfd0c4d1a3100d3850078d2befd5e6be414d0e
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-06-26 18:04:34 +0400

    chapter-7: CustomersSearch uses regexp to get nextpage and prevpage urls

--- src/views/CustomersSearch/NavigationButtons.tsx
+++ src/views/CustomersSearch/NavigationButtons.tsx
@@ -1,15 +1,14 @@
 import React from "react"
-import { Link, useSearchParams } from "react-router-dom"
+import { Link, useLocation, useSearchParams } from "react-router-dom"
 
 export const NavigationButtons: React.FC = ({}) => {
   const [searchParams] = useSearchParams()
 
+  const location = useLocation()
+
   const pageNumberSearchParam = searchParams.get("page")
-  const searchTermSearchParam = searchParams.get("searchTerm")
 
   if (pageNumberSearchParam === null) return null
-
-  const pageNumber = parseInt(pageNumberSearchParam)
 
   return (
     <div className="button-bar">
@@ -17,9 +16,9 @@ export const NavigationButtons: React.FC = ({}) => {
         <Link
           className="bg-red-200 hover:bg-red-400"
           id="previous-page"
-          to={`/customers-search?page=${pageNumber - 1}${
-            searchTermSearchParam ? `&searchTerm=${searchTermSearchParam}` : ""
-          }`}
+          to={`${location.pathname}${location.search}`.replace(/(?<=page=)\d+/, (aMatch) =>
+            String(parseInt(aMatch) - 1)
+          )}
         >
           Previous
         </Link>
@@ -27,9 +26,7 @@ export const NavigationButtons: React.FC = ({}) => {
       <Link
         className="bg-cyan-200 hover:bg-cyan-400"
         id="next-page"
-        to={`/customers-search?page=${pageNumber + 1}${
-          searchTermSearchParam ? `&searchTerm=${searchTermSearchParam}` : ""
-        }`}
+        to={`${location.pathname}${location.search}`.replace(/(?<=page=)\d+/, (aMatch) => String(parseInt(aMatch) + 1))}
       >
         Next
       </Link>
